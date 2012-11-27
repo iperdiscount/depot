d0206e8beff787ec403bcd7c436a8a202ea1af67
Iperdiscount <iperdiscount@gmail.com>
2012-11-28 00:14:33 +0100

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ebd5b61edce62ec22ec789e4f1fa633825166d0601d68f8a71393eada6e78264e9b2d3d7317583fe113615e40fd2a9eea81fa2b343c519519c56add762703335'
